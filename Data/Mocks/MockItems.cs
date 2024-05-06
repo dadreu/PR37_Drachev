8a55b488efc1fca05121816d5a8d05da755fb2db
Dadreu <dadreu@gmail.com>
2024-05-06 23:24:00 +0500

    Разработанное практическое занятие на оценку удовлетворительно. Драчёв

--- Data/Mocks/MockItems.cs
+++ Data/Mocks/MockItems.cs
@@ -23,7 +23,7 @@ namespace Shop_Drachev.Data.Mocks
                         Id = 0,
                         Name = "DEXP MS-70",
                         Description = "Черный корпус",
-                        Img = "",
+                        Img = "https://c.dns-shop.ru/thumb/st1/fit/300/300/f67706d3e9f41fdc37fbf1d4ea715b35/b1a761fddbd2197e22bdcf5ee0cd1cfd773ce824ab6ef6eba7411b9a626c50a7.jpg.webp",
                         Price = 3369,
                         Category = _category.AllCategorys.Where(x => x.Id == 0).First() 
                     } 
