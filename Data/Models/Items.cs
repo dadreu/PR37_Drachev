8f235baf9880c815e59a956ee1263849c7f0198a
student-a210 <student-a210@PC-A210-7>
2024-05-06 15:59:35 +0500

    Разработанные модели данных. Драчёв

---
+++ Data/Models/Items.cs
@@ -0,0 +1,14 @@
+﻿namespace Shop_Drachev.Data.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public int Price { get; set; }
+        public Categorys Category { get; set; }
+
+        
+    }
+}
