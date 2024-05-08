e324ab7d24e6a6837dbc24a8d5561661cf14169a
student-a310 <student-a310@PC-A310-11>
2024-05-08 13:52:37 +0500

    Загрузить изменения в систему контроля версий, указать комментарий «Получение данных о категориях. Драчёв

---
+++ Data/DataBase/DBCategory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Shop_Drachev.Data.Common;
+using Shop_Drachev.Data.Models;
+using Shop_Drachev.Data.Interfaces;
+
+namespace Shop_Drachev.Data.DataBase
+{
+    public class DBCategory : ICategorys
+    {
+        public IEnumerable<Categorys> AllCategorys
+        {
+            get
+            {
+                List<Categorys> foundCategorys = new List<Categorys>();
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+                MySqlDataReader CategorysData = Connection.MySqlQuery("SELECT * FROM Shop.Categorys ORDER BY `Name`;", MySqlConnection);
+                while (CategorysData.Read())
+                {
+                    foundCategorys.Add(new Categorys()
+                    {
+                        Id = CategorysData.IsDBNull(0) ? -1 : CategorysData.GetInt32(0),
+                        Name = CategorysData.IsDBNull(1) ? null : CategorysData.GetString(1),
+                        Description = CategorysData.IsDBNull(2) ? null : CategorysData.GetString(2)
+                    });
+                }
+                return foundCategorys;
+            }
+        }
+    }
+}
