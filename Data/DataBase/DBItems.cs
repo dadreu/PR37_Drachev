388fc5f9786e15f4029095a6701c21fb8dd43aff
student-a310 <student-a310@PC-A310-11>
2024-05-08 14:00:18 +0500

    Получение данных о предметах. Драчёв

---
+++ Data/DataBase/DBItems.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Shop_Drachev.Data.Common;
+using Shop_Drachev.Data.Models;
+using Shop_Drachev.Data.Interfaces;
+using Shop_Drachev.Data.DataBase;
+
+namespace Shop_Тепляков.Data.DataBase
+{
+    public class DBItems : IItems
+    {
+        public IEnumerable<Categorys> Categorys = new DBCategory().AllCategorys;
+
+        public IEnumerable<Items> AllItems
+        {
+            get
+            {
+                List<Items> items = new List<Items>();
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+                MySqlDataReader ItemsReader = Connection.MySqlQuery("Select * from Items Order By 'Name';", MySqlConnection);
+                while (ItemsReader.Read())
+                {
+                    items.Add(new Items()
+                    {
+                        Id = ItemsReader.IsDBNull(0) ? -1 : ItemsReader.GetInt32(0),
+                        Name = ItemsReader.IsDBNull(1) ? null : ItemsReader.GetString(1),
+                        Description = ItemsReader.IsDBNull(2) ? null : ItemsReader.GetString(2),
+                        Img = ItemsReader.IsDBNull(3) ? null : ItemsReader.GetString(3),
+                        Price = ItemsReader.IsDBNull(4) ? -1 : ItemsReader.GetInt32(4),
+                        Category = ItemsReader.IsDBNull(5) ? null : Categorys.First(x => x.Id == ItemsReader.GetInt32(5))
+                    });
+                }
+                MySqlConnection.Close();
+                return items;
+            }
+        }
+    }
+}
