346373926944026c852ce87aa43f56cd65c18982
student-a310 <student-a310@PC-A310-11>
2024-05-08 13:40:34 +0500

    Загрузить изменения в систему контроля версий, указать комментарий «Разработанное подключение к MySql. Драчёв

---
+++ Data/Comon/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Shop_Drachev.Data.Common
+{
+    public class Connection
+    {
+        readonly static string ConnectionData = "server=127.0.0.1;port=3306;database=need;uid=root;";
+
+        public static MySqlConnection MySqlOpen()
+        {
+            MySqlConnection newConnection = new MySqlConnection(ConnectionData);
+            newConnection.Open();
+            return newConnection;
+        }
+
+        public static MySqlDataReader MySqlQuery(string Query, MySqlConnection Connection)
+        {
+            MySqlCommand newCommand = new MySqlCommand(Query, Connection);
+            return newCommand.ExecuteReader();
+        }
+
+        public static void MySqlClose(MySqlConnection Connection)
+        {
+            Connection.Close();
+        }
+    }
+}
