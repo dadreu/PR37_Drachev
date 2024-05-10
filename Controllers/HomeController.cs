1a250c5b55234c0b5d90f27417d2c2d58196715b
Dadreu <dadreu@gmail.com>
2024-05-10 17:25:16 +0500

    Разработанное перенаправление на страницу с предметами. Драчёв

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop_Drachev.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RedirectResult Index()
+        {
+            return Redirect("Items/List");
+        }
+    }
+}
